75a645db2515050c9259304ecd88943128be1a1f
prernasehgal12 <143725851+prernasehgal12@users.noreply.github.com>
2023-10-19 20:18:05 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -6,9 +6,9 @@ module "ec2" {
   subnet_id = module.vpc.subnet_id
   tags = merge(var.tags,{Name = each.key})
   volume_tags = merge(var.volume_tags,{Name = each.key})
-(*   #owner = each.value.owner
+  #owner = each.value.owner
   #name = each.key
-  #purpose = each.value.purpose *)
+  #purpose = each.value.purpose 
   security_group_id    = module.security_group.sg_id
   
 }
