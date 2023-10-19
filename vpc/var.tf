f44bfbfed80a72d7125bfe34a2d9ef41fe13372c
prernasehgal12 <143725851+prernasehgal12@users.noreply.github.com>
2023-10-19 17:54:26 +0530

    commit

---
+++ vpc/var.tf
@@ -0,0 +1,16 @@
+variable "tags" {
+    type = map(string)
+  
+}
+variable "cidr_block" {
+    type = string
+  
+}
+variable "instance_tenancy" {
+    type = string
+  
+}
+variable "vpc_id" {
+    type = string
+  
+}
